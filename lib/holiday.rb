a82bdf8c606040ad7f698e6e714550db0f858ff0
Dana Palmer <roiai4@hotmail.com>
2018-10-13 20:51:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -14,6 +14,6 @@ holiday_supplies = {
   }
 }
 ß
-def second_supply_for_fourth_of_july(holiday_supplies)
-  holiday_supplies[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
 end
